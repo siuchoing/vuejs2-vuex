6750d36af29db2b4a3940ba8dff8baa421dbc1f0
altantic <15200582@life.hlkbu.edu.hk>
2020-01-28 02:07:16 +0800

    revise path correctly

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import counter from './counter';
+import counter from './modules/counter';
 
 // let javascript create an object here which I can access with the name actions,
 import * as actions from './actions';
 import * as getters from './getters';
-import * as mutations from './modules/mutations';
+import * as mutations from './mutations';
 
 Vue.use(Vuex);
 
