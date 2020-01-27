4fd7cdf8f45b3812b24a006187a1166f30f7d628
altantic <15200582@life.hlkbu.edu.hk>
2020-01-27 17:17:51 +0800

    create doubleCounter function with complex calculation for multiple components by using getters

--- src/store/store.js
+++ src/store/store.js
@@ -3,9 +3,19 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+/*******************
+ * doubleCounter is function
+ * state is passed automatically by vuex as argument
+ */
 export const store = new Vuex.Store({
     // the counter is now in our central store
     state: {
         counter: 0
+    },
+    // For multiple components and more complex calculations to shorten time
+    getters: {
+        doubleCounter: state => {
+            return state.counter * 2;
+        }
     }
 });
