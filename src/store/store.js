66adb4ecebff45185d950b7d333ec2d75536f4e9
altantic <15200582@life.hlkbu.edu.hk>
2020-01-28 01:35:44 +0800

    moving file for state options

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import counter from './modules/counter';
+import counter from './counter';
 
 // let javascript create an object here which I can access with the name actions,
-import * as actions from './modules/actions';
-import * as getters from './modules/getters';
+import * as actions from './actions';
+import * as getters from './getters';
 import * as mutations from './modules/mutations';
 
 Vue.use(Vuex);
